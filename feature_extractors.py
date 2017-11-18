9c251ced1aa336f53be20f25dc317cf7675c846d
Glenn Yu <glennyu@DN51us9l.SUNet>
2017-11-17 21:24:09 -0800

    features

--- feature_extractors.py
+++ feature_extractors.py
@@ -1,10 +1,17 @@
 from collections import defaultdict
 from game import DECK_CARD_VALUES, ROW_LENGTHS
+import itertools
+import numpy as np
 import random
 
-
 SUITS = 'CDHS'
+DECK_SIZE = 52
+CARDS_PER_VALUE = 4
+CARDS_PER_SUIT = 13
+MIN_DECK_SIZE = 22
+
 card_to_value = dict() # relative ordering of cards (2 is lowest, A is highest)
+probability_lookup_table = defaultdict(float)
 
 # Converts an array into a map from array value to its count
 def get_frequency_map(arr):
@@ -25,6 +32,159 @@ def monte_carlo_sim(deck, num_to_draw, satisfies_hand, needed):
 			success_cnt += 1
 	return 1.0*success_cnt/num_trials
 
+
+def precompute_hand_probs():
+	for i, card in enumerate(DECK_CARD_VALUES):
+		card_to_value[card] = i
+	full_deck = set([a + b for a, b in itertools.product(DECK_CARD_VALUES, 'CDHS')])
+	reference_card_values = ['A', '2', '3', '4' , '5'] # arbitrary values used to construct deck
+
+	# Single / Pair / Triple / Four of a kind
+	# probability_lookup_table -- key: (target_freq, row_freq, deck_freq, num_to_draw, deck_size)
+	#								   all ints except for card_value (string)
+	def satisfies_hand(chosen_cards, needed):
+		card_value, num_cards_needed = needed
+		chosen = [card[0] for card in chosen_cards]
+		cnt = chosen.count(card_value)
+		return (cnt >= num_cards_needed)
+
+	print 'head'
+	
+	output_file = open('cardinality_probabilities.txt', 'w')
+	parameters = ['Probability', 'Target freq', 'Row freq', 'Deck freq', 'Number to draw', 'Deck size']
+	output_file.write('\t'.join(parameters) + '\n')
+	
+	card_value = reference_card_values[0] 
+	for row_freq in range(ROW_LENGTHS[1] + 1):
+		deck_freq = CARDS_PER_VALUE - row_freq
+		for deck_size in range(MIN_DECK_SIZE, DECK_SIZE + 1):
+			# Build customized deck
+			deck = full_deck.copy()
+			deck -= set([a + b for a, b in itertools.product(card_value, 'CDHS')])
+			deck = set(list(deck)[:(deck_size - deck_freq)])
+			for i in range(deck_freq):
+				deck.add(card_value + SUITS[i])
+
+			for num_to_draw in range(sum(ROW_LENGTHS) + 1):
+				for target_freq in range(1, 5):
+					if (row_freq >= target_freq):
+						prob = 1.0
+					elif (num_to_draw == 0):
+						prob = 0.0
+					else:
+						num_cards_needed = target_freq - row_freq
+						if (num_cards_needed > deck_freq): 
+							continue
+						needed = (card_value, num_cards_needed)
+						prob = monte_carlo_sim(deck, num_to_draw, satisfies_hand, needed)
+
+					probability_lookup_table[(target_freq, row_freq, deck_freq, num_to_draw, deck_size)] = prob
+					output_file.write('\t'.join([str(x).rjust(9) for x in [prob, target_freq, row_freq, deck_freq, num_to_draw, deck_size]]) + '\n')
+	output_file.close()
+
+	print 'straight'
+
+	# Straight
+	# probability_lookup_table -- key: (sorted_multiplicity, num_to_draw, deck_size)
+	#								   (tuple, int, int)
+	def satisfies_straight(chosen_cards, needed):
+		chosen = set(card[0] for card in chosen_cards)
+		for val in needed:
+			if (val not in chosen):
+				return False
+		return True
+
+	output_file = open('straight_probabilities.txt', 'w')
+	parameters = ['probability', 'multiplicity', 'num_to_draw', 'deck_size']
+	output_file.write('\t'.join(parameters) + '\n')
+
+	for num_empty_space in range(ROW_LENGTHS[1], 0, -1):
+		possibilities = []
+		for i in range(1, 5):
+			for j in range(num_empty_space):
+				possibilities.append(i)
+		multiplicities = set(itertools.combinations(possibilities, num_empty_space))
+		for multiplicity in multiplicities:
+			deck_freq = sum(multiplicity)
+			for deck_size in range(MIN_DECK_SIZE, DECK_SIZE + 1):
+				# Build customized deck
+				deck = full_deck.copy()
+				deck -= set([a + b for a, b in itertools.product(reference_card_values[:num_empty_space], 'CDHS')])
+				deck = set(list(deck)[:(deck_size - deck_freq)])	
+				needed = []
+				for idx in range(len(multiplicity)):
+					val = multiplicity[idx]
+					for suit_idx in range(val):
+						deck.add(reference_card_values[idx] + SUITS[suit_idx])
+					needed.append(reference_card_values[idx])
+
+				for num_to_draw in range(num_empty_space, sum(ROW_LENGTHS) + 1):
+					prob = monte_carlo_sim(deck, num_to_draw, satisfies_straight, set(needed))
+					probability_lookup_table[(multiplicity, num_to_draw, deck_size)] = prob
+					output_file.write('\t'.join([str(x).rjust(10) for x in [prob, multiplicity, num_to_draw, deck_size]]) + '\n')
+	output_file.close()
+
+	print 'Flush'
+
+	# Flush
+	# probability_lookup_table -- key: (high_card, in_cards, num_empty_space, num_suit_left, num_to_draw, deck_size)
+	#								   all ints except for high_Card (string) and in_cards (bool) which indicates whether high card is in row
+	def satisfies_flush(chosen_cards, needed):
+		chosen_card_values = [DECK_CARD_VALUES.index(card[0]) for card in chosen_cards]
+		chosen_card_suits = [card[1] for card in chosen_cards]
+		sorted_suits = [x for _,x in sorted(zip(chosen_card_values, chosen_card_suits))]
+		feature_value, num_cards_needed, high_card_in_cards = needed
+		high_card_name = feature_value[0]
+		high_card_value = card_to_value[high_card_name]
+		flush_suit = feature_value[1]
+		high_card = high_card_name + flush_suit
+
+		if (chosen_card_suits.count(flush_suit) < num_cards_needed):
+			return False
+		if (high_card_in_cards):
+			helper_arr = chosen_card_values + [high_card_value]
+			idx = sorted(helper_arr).index(high_card_value)
+			if (sorted_suits[:idx].count(flush_suit) < num_cards_needed):
+				return False
+		elif (high_card not in chosen_cards):
+			return False
+		else:
+			idx = sorted(chosen_card_values).index(high_card_value)
+			if ((sorted_suits[:idx]).count(flush_suit) < num_cards_needed):
+				return False
+		return True
+
+	output_file = open('flush_probabilities.txt', 'w')
+	parameters = ['probability', 'high_card', 'in_cards', 'num_empty_space', 'num_suit_left', 'num_to_draw', 'deck_size']
+	output_file.write('\t    '.join(parameters) + '\n')
+
+	reference_suit = 'C'
+	for num_empty_space in range(1, ROW_LENGTHS[1] + 1):
+		for num_suit_left in range(num_empty_space, CARDS_PER_SUIT + 1):
+			for deck_size in range(MIN_DECK_SIZE, DECK_SIZE + 1):
+				for high_card in DECK_CARD_VALUES[3:]: # min high card value of '5'
+					feature_value = high_card + reference_suit
+					needed = (feature_value, num_empty_space)
+					for in_cards in range(2): # False or True
+						# Build customized deck
+						deck = full_deck.copy()
+						deck -= set([a + b for a, b in itertools.product(reference_card_values[-num_suit_left:], 'C')])
+						deck = set(list(deck)[:(deck_size - num_suit_left)])
+						valid_card_values = DECK_CARD_VALUES
+						if (in_cards):
+							valid_card_values = valid_card_values.replace(high_card, '')
+							valid_card_values = list(valid_card_values[ROW_LENGTHS[1] - num_empty_space - 1:])
+						else:
+							valid_card_values = list(valid_card_values[ROW_LENGTHS[1] - num_empty_space:])
+						card_combinations = [a + b for a, b in itertools.product(np.random.choice(valid_card_values, num_suit_left), reference_suit)]
+						deck |= set(card_combinations)
+						
+						for num_to_draw in range(num_empty_space, sum(ROW_LENGTHS) + 1):
+							prob = monte_carlo_sim(deck, num_to_draw, satisfies_flush, needed + (in_cards,))
+							probability_lookup_table[(high_card, in_cards, num_empty_space, num_suit_left, num_to_draw, deck_size)] = prob
+							output_file.write('\t'.join([str(x).rjust(13) for x in [prob, high_card, in_cards, num_empty_space, num_suit_left, num_to_draw, deck_size]]) + '\n')
+	output_file.close()
+
 # Returns a monte-carlo simulated hand probability feature vector of a given row
 # Inputs:
 #	row_num 	[int]: row number
@@ -40,39 +200,33 @@ def monte_carlo_sim(deck, num_to_draw, satisfies_hand, needed):
 #	Value: 	probability of hand_name at card_value 
 def feature_extractor_1(row_num, cards, deck, num_to_draw):
 	features = defaultdict(float)
-	card_freq = get_frequency_map([card[0] for card in cards])
-	for i, card in enumerate(DECK_CARD_VALUES):
-		card_to_value[card] = i
+	row_card_freq = get_frequency_map([card[0] for card in cards])
+	deck_card_freq = get_frequency_map([card[0] for card in deck])
 	num_cards = len(cards)
-	num_cards_left = ROW_LENGTHS[row_num] - num_cards
-	assert(num_to_draw >= num_cards_left)
+	num_empty_space = ROW_LENGTHS[row_num] - num_cards
+	deck_size = len(deck)
+	assert(num_to_draw >= num_empty_space)
 
 	# Single / Pair / Triple / Four of a kind
-	def satisfies_hand(chosen_cards, needed):
-		card_value, num_cards_needed = needed
-		chosen = [card[0] for card in chosen_cards]
-		cnt = chosen.count(card_value)
-		return (cnt >= num_cards_needed)
-
 	for card_value in DECK_CARD_VALUES:
-		freq = card_freq[card_value]
+		row_freq = row_card_freq[card_value]
+		deck_freq = deck_card_freq[card_value]
+		assert(deck_freq + row_freq == 4) 
 		for target_freq in range(1, 5):
 			if (target_freq == 4 and row_num == 0): 
 				continue
 
-			if (freq >= target_freq):
-				prob = 1.0
-			else:
-				num_cards_needed = target_freq - freq
-				if (num_cards_needed > num_cards_left): 
-					continue
-				needed = (card_value, num_cards_needed)
-				prob = monte_carlo_sim(deck, num_to_draw, satisfies_hand, needed)
-
-			features[(str(target_freq), card_value)] = prob
+			features[(str(target_freq), card_value)] = \
+				probability_lookup_table[(target_freq, row_freq, deck_freq, num_to_draw, deck_size)]
 
 	# Straight / Flush
 	if (row_num > 0):
+		def get_multiplicity(deck_card_freq, needed):
+			multiplicity = []
+			for needed_card in needed:
+				multiplicity.append(deck_card_freq[needed_card])
+			return tuple(sorted(multiplicity))
+
 		# Straight
 		card_values = [DECK_CARD_VALUES.index(card[0]) for card in cards]
 		card_suits = [card[1] for card in cards]
@@ -86,13 +240,6 @@ def feature_extractor_1(row_num, cards, deck, num_to_draw):
 					return False
 			return True
 
-		def satisfies_straight(chosen_cards, needed):
-			chosen = [card_to_value[card[0]] for card in chosen_cards]
-			for val in needed:
-				if (val not in chosen):
-					return False
-			return True
-
 		if (firstStraight(card_values)):
 			needed = []
 			for i in range(card_to_value['2'], card_to_value['5'] + 1):
@@ -100,7 +247,8 @@ def feature_extractor_1(row_num, cards, deck, num_to_draw):
 					needed.append(i)
 			if (card_to_value['A'] not in card_values): # Ace
 				needed.append(card_to_value['A'])
-			features[('St', '5')] = monte_carlo_sim(deck, num_to_draw, satisfies_straight, needed)
+			multiplicity = get_multiplicity(deck_card_freq, needed)
+			features[('St', '5')] = probability_lookup_table[(multiplicity, num_to_draw, deck_size)]
 
 		if (max_value - min_value < 5):
 			for high_card in range(max(max_value, card_to_value['6']), min(min_value + 5, card_to_value['A'] + 1)):
@@ -108,44 +256,21 @@ def feature_extractor_1(row_num, cards, deck, num_to_draw):
 				for i in range(high_card - 4, high_card + 1):
 					if (i not in card_values):
 						needed.append(i)
-				features[('St', DECK_CARD_VALUES[high_card])] = monte_carlo_sim(deck, num_to_draw, satisfies_straight, needed)
+				multiplicity = get_multiplicity(deck_card_freq, needed)
+				features[('St', DECK_CARD_VALUES[high_card])] = probability_lookup_table[(multiplicity, num_to_draw, deck_size)]
 
 		# Flush
 		for suit in SUITS:
+			deck_suits = [card[1] for card in deck]
+			deck_suit_count = deck_suits.count(suit)
 			if (card_suits.count(suit) == num_cards):
 				for card_name in DECK_CARD_VALUES:
 					if (card_to_value[card_name] < max_value):
 						continue
-					num_cards_needed = ROW_LENGTHS[row_num] - num_cards
 					feature_value = card_name + suit
-					needed = (feature_value, num_cards_needed)
-
-					def satisfies_flush(chosen_cards, needed):
-						chosen_card_values = [DECK_CARD_VALUES.index(card[0]) for card in chosen_cards]
-						chosen_card_suits = [card[1] for card in chosen_cards]
-						sorted_suits = [x for _,x in sorted(zip(chosen_card_values, chosen_card_suits))]
-						feature_value, num_cards_needed = needed
-						high_card_name = feature_value[0]
-						high_card_value = card_to_value[high_card_name]
-						flush_suit = feature_value[1]
-						high_card = high_card_name + flush_suit
-
-						if (chosen_card_suits.count(flush_suit) < num_cards_needed):
-							return False
-						if (high_card in cards):
-							helper_arr = chosen_card_values + [high_card_value]
-							idx = sorted(helper_arr).index(high_card_value)
-							if (sorted_suits[:idx].count(flush_suit) < num_cards_needed):
-								return False
-						elif (high_card not in chosen_cards):
-							return False
-						else:
-							idx = sorted(chosen_card_values).index(high_card_value)
-							if ((sorted_suits[:idx]).count(flush_suit) < num_cards_needed):
-								return False
-						return True
-
-					features[('Fl', feature_value)] = monte_carlo_sim(deck, num_to_draw, satisfies_flush, needed)
+					in_cards = (card_name in cards)
+					num_cards_needed = ROW_LENGTHS[row_num] - num_cards
+					features[('Fl', feature_value)] = probability_lookup_table[(card_name, in_cards, num_cards_needed, deck_suit_count, num_to_draw, deck_size)]
 
 	return features
 
--- feature_extractors_test.py
+++ feature_extractors_test.py
@@ -1,87 +1,16 @@
 from feature_extractors import *
+from game import DECK_CARD_VALUES
+import itertools
 
 def row_0_tests():
 	# Testing single / pair / triple
-	features = feature_extractor_1(0, ['AS', '4C'], ['AC', 'AH', 'AD'], 2)
+	features = feature_extractor_1(0, ['AS', '5C'], full_deck - set(['AS', '5C']), 2)
 	assert features[('1', 'A')] == 1.0
-	assert features[('1', '4')] == 1.0
-	assert features[('2', 'A')] == 1.0
+	assert features[('1', '5')] == 1.0
+	assert features[('2', 'A')] < 1.0
 	assert features[('3', '4')] == 0.0
 	assert features[('St', '5')] == 0.0
 
-	features = feature_extractor_1(0, ['4S', '4C'], ['AH', 'AD', '4D', '5C', '6D'], 3)
-	assert features[('1', 'A')] > 0.0
-	assert features[('1', '4')] == 1.0
-	assert features[('2', '4')] == 1.0
-	assert features[('2', 'A')] == 0.0
-	assert features[('3', '4')] > 0.0
-
-	features = feature_extractor_1(0, [], ['AH', 'AD', 'AC', '4D', '6C', '6D'], 3)
-	assert features[('1', 'A')] > 0.0
-	assert features[('1', '4')] > 0.0
-	assert features[('2', '6')] > 0.0
-	assert features[('2', '4')] == 0.0
-	assert features[('3', 'A')] > 0.0
-
-def last_two_row_tests():
-	# Testing single / pair / triple / four fo a kind
-	features = feature_extractor_1(1, ['AS', '4C'], ['AC', 'AH', 'AD'], 3)
-	assert features[('1', 'A')] == 1.0
-	assert features[('1', '4')] == 1.0
-	assert features[('2', 'A')] == 1.0
-	assert features[('4', 'A')] == 1.0
-	assert features[('3', '4')] == 0.0
-	assert features[('St', '5')] == 0.0
-
-	features = feature_extractor_1(1, ['AS'], ['AH', 'AD', 'AC', '4D', '6C', '6D'], 4)
-	assert features[('1', 'A')] == 1.0
-	assert features[('1', '4')] > 0.0
-	assert features[('2', '6')] > 0.0
-	assert features[('2', '4')] == 0.0
-	assert features[('3', 'A')] > 0.0
-	assert features[('4', 'A')] > 0.0
-
-	# Testing straight
-	features = feature_extractor_1(1, ['2S', '4C'], ['AC', '2H', '3D', '4D', '5C', '6D'], 4)
-	assert features[('2', '4')] > 0.0
-	assert features[('2', 'A')] == 0.0
-	assert features[('3', '4')] == 0.0
-	assert features[('St', '5')] > 0.0
-	assert features[('St', '6')] > 0.0
-	assert features[('St', '7')] == 0.0
-
-	features = feature_extractor_1(2, ['8D'], ['4C', '5H', '5S', '6D', '7S', '9D', 'TD', 'JC', 'QD', 'KS'], 5)
-	assert features[('St', '8')] > 0.0
-	assert features[('St', '9')] > 0.0
-	assert features[('St', 'T')] > 0.0
-	assert features[('St', 'J')] > 0.0
-	assert features[('St', 'Q')] > 0.0
-	assert features[('St', 'K')] == 0.0
-
-	# Testing flush
-	features = feature_extractor_1(2, ['8D', 'TD'], ['4D', '5D', '5C', '6D', '7D', '9D', 'JD', 'JC', 'QD', 'KS'], 5)
-	assert features[('Fl', '8S')] == 0.0
-	assert features[('Fl', 'AC')] == 0.0
-	assert features[('Fl', 'KD')] == 0.0
-	assert features[('Fl', 'JH')] == 0.0
-	assert features[('Fl', 'TD')] > 0.0
-	assert features[('Fl', 'QD')] > 0.0
-
-	features = feature_extractor_1(2, ['8D', 'TS'], ['4D', '5D', '5C', '6D', '7D', '9D', 'JD', 'JC', 'QD', 'KS'], 5)
-	assert features[('Fl', '8S')] == 0.0
-	assert features[('Fl', 'AC')] == 0.0
-	assert features[('Fl', 'KD')] == 0.0
-	assert features[('Fl', 'JH')] == 0.0
-
-	features = feature_extractor_1(2, ['5S', '7S', '8S'], ['4S', '5D', '5C', '6D', '9S', 'JD', 'JS', 'QD', 'KS', 'AS'], 4)
-	assert features[('Fl', '8S')] == 0.0
-	assert features[('Fl', 'AC')] == 0.0
-	assert features[('Fl', 'KD')] == 0.0
-	assert features[('Fl', '9S')] == 0.0
-	assert features[('Fl', 'JS')] > 0.0
-	assert features[('Fl', 'QS')] == 0.0
-	assert features[('Fl', 'KS')] > 0.0
-	assert features[('Fl', 'AS')] > 0.0
-
+full_deck = set([a + b for a, b in itertools.product(DECK_CARD_VALUES, 'CDHS')])
+precompute_hand_probs()
 row_0_tests()
-last_two_row_tests()
