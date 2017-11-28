193b5f28b787f18df7ef48e64564e84ddbf3e656
Vinson Luo <vluo@stanford.edu>
2017-11-27 23:24:31 -0800

    Separate out testing code into hand_optimizer_test.

--- hand_optimizer.py
+++ hand_optimizer.py
@@ -1,7 +1,6 @@
 from collections import defaultdict
 import itertools
 import random
-import time
 
 import game as g
 
@@ -324,33 +323,3 @@ def optimize_hand(rows, draw, return_combo=False):
   if return_combo:
     return g.BUST_PENALTY, None
   return g.BUST_PENALTY
-
-# Testing
-
-# print possible_hands(['TH', '9H'], 1, ['2H', '2S', 'JC', 'QC', '8C', '7H', 'AH'])
-# print possible_hands(['TH', '5S'], 1, ['2H', '2S', 'JC', '8C', '7H', '9D', '5C', '5D'])
-# print possible_hands([], 1, ['2H', '2S', 'JC', '8C', '7H', 'TH', 'JH', 'QH', 'TC', 'TD', '8S', '8H'])
-# print possible_hands(['TD', 'TS'], 1, ['2H', '2S', 'JC', '8C', '7H', 'TH', 'JH', 'QH'])
-# print possible_hands(['TD', 'TS', '3C', 'QD'], 1, ['2H', '2S', 'JC', '8C', '7H', 'TH', 'JH', 'QH'])
-# print possible_hands(['TD', 'TS', 'QS', 'QD'], 1, ['2H', '2S', 'JC', '8C', '7H', 'TH', 'JH', 'QH'])
-# print possible_hands(['TD', 'TS', 'TC'], 1, ['2H', '2S', 'JC', '8C', '7H', 'TH', 'JH', 'QH'])
-# print possible_hands([], 0, ['2H', '2S', 'JC', '8C', '7H', 'TH', 'JH', 'QH', 'JS', '7C'])
-# print possible_hands([], 0, ['2H', '2C', 'JH', 'JD', 'JS'])
-# print possible_hands([], 2, ['2H', '2C', 'JH', 'JD', 'JS'])
-# print possible_hands([], 1, ['2H', '2S', 'JC', '8C', '7H', 'TH', 'JH', 'QH', 'JS', '7C'])
-
-# start = time.time()
-# for _ in xrange(1000):
-#   #possible_hands([], 1, ['2H', '2S', 'JC', '8C', '7H', 'TH', 'JH', 'QH', 'TC', 'TD', '8S', '8H'])
-#   optimize_hand([['TH', 'TC'], ['2H', '2D', '3S', '4C'], ['7S', '8S', '9S', 'TS']], ['TD', 'JC', 'QS', '2C', '3C', '4D'])
-# print "Average time for possible_hands: {}".format((time.time() - start) / 1000.)
-
-# print optimize_hand([['TH', 'TC'], ['2H', '2D', '3S', '4C'], ['8S', '9S', 'TS']], ['7S', '7D', 'TD', 'JC', 'QS', '2C', '3C', '4D'])
-# print optimize_hand([['TH', 'TC'], ['2H', '2D', '3S', '4C'], ['8S', '8C', 'TS']], ['7S', '7D', 'TD', 'JC', 'QS', '2C', '3C', '4D', '8H', 'TH'])
-# print optimize_hand([['TC'], ['2H', '2D', '3S', '4C'], ['8S', '8C', 'TS']], ['7S', '7D', 'TD', 'JC', 'QS', '2C', '3C', '4D', '8H', 'TH', '8D'], True)
-# print optimize_hand([['TC', 'AH'], ['2H', '2D', '3S', '4C'], ['8S', '8C', 'TS', 'TD']], ['TH', '2S', '3H'], True)
-
-game = g.PineappleGame1()
-draw = random.sample(game.cards, 14)
-print "Random draw:", g.sort_cards(draw)
-print optimize_hand([[], [], []], draw, True)
---
+++ hand_optimizer_test.py
@@ -0,0 +1,28 @@
+from hand_optimizer import *
+
+import time
+
+# Testing
+
+# print possible_hands(['TH', '9H'], 1, ['2H', '2S', 'JC', 'QC', '8C', '7H', 'AH'])
+# print possible_hands(['TH', '5S'], 1, ['2H', '2S', 'JC', '8C', '7H', '9D', '5C', '5D'])
+# print possible_hands([], 1, ['2H', '2S', 'JC', '8C', '7H', 'TH', 'JH', 'QH', 'TC', 'TD', '8S', '8H'])
+# print possible_hands(['TD', 'TS'], 1, ['2H', '2S', 'JC', '8C', '7H', 'TH', 'JH', 'QH'])
+# print possible_hands(['TD', 'TS', '3C', 'QD'], 1, ['2H', '2S', 'JC', '8C', '7H', 'TH', 'JH', 'QH'])
+# print possible_hands(['TD', 'TS', 'QS', 'QD'], 1, ['2H', '2S', 'JC', '8C', '7H', 'TH', 'JH', 'QH'])
+# print possible_hands(['TD', 'TS', 'TC'], 1, ['2H', '2S', 'JC', '8C', '7H', 'TH', 'JH', 'QH'])
+# print possible_hands([], 0, ['2H', '2S', 'JC', '8C', '7H', 'TH', 'JH', 'QH', 'JS', '7C'])
+# print possible_hands([], 0, ['2H', '2C', 'JH', 'JD', 'JS'])
+# print possible_hands([], 2, ['2H', '2C', 'JH', 'JD', 'JS'])
+# print possible_hands([], 1, ['2H', '2S', 'JC', '8C', '7H', 'TH', 'JH', 'QH', 'JS', '7C'])
+
+# start = time.time()
+# for _ in xrange(1000):
+#   #possible_hands([], 1, ['2H', '2S', 'JC', '8C', '7H', 'TH', 'JH', 'QH', 'TC', 'TD', '8S', '8H'])
+#   optimize_hand([['TH', 'TC'], ['2H', '2D', '3S', '4C'], ['7S', '8S', '9S', 'TS']], ['TD', 'JC', 'QS', '2C', '3C', '4D'])
+# print "Average time for possible_hands: {}".format((time.time() - start) / 1000.)
+
+# print optimize_hand([['TH', 'TC'], ['2H', '2D', '3S', '4C'], ['8S', '9S', 'TS']], ['7S', '7D', 'TD', 'JC', 'QS', '2C', '3C', '4D'])
+# print optimize_hand([['TH', 'TC'], ['2H', '2D', '3S', '4C'], ['8S', '8C', 'TS']], ['7S', '7D', 'TD', 'JC', 'QS', '2C', '3C', '4D', '8H', 'TH'])
+# print optimize_hand([['TC'], ['2H', '2D', '3S', '4C'], ['8S', '8C', 'TS']], ['7S', '7D', 'TD', 'JC', 'QS', '2C', '3C', '4D', '8H', 'TH', '8D'], True)
+# print optimize_hand([['TC', 'AH'], ['2H', '2D', '3S', '4C'], ['8S', '8C', 'TS', 'TD', '8D']], ['TH', 'AS', 'AC'], True)
